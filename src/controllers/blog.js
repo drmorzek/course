b350a5312b9025be1fbd0eaf94823d32e8f93e8a
drmorzek <drmorzek@mail.ru>
2020-05-12 09:39:05 +0300

    корректировка контроллера

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -4,22 +4,36 @@ const blogs = require("../models/blog");
 //колбек функции для блогов
 //============================
 //получить все блоги
-exports.getAll = (req, res) => {
-    blogs.find()
+const getAll = async (req, res) => {
+    await blogs
+        .find()
         .exec((err, data) => {
-        if (err) throw err;
-        res.send(data);
-    });
-};
+            if (err) throw err;
+            if (data !== []) {
+                res.status(200).send(data);
+            } else {
+                res.sendStatus(501);
+            }
+        });
+    };
 
 //получить блог по id
-exports.getOne = (req, res) => {
-    blogs
+const getOne = async (req, res) => {
+    await blogs
         .find({
             id: Number(req.params.id),
         })
         .exec((err, data) => {
             if (err) throw err;
-            res.send(data);
+            if (data !== []) {
+                res.status(200).send(data);
+            } else {
+                res.sendStatus(501);
+            }
         });
-};
+    };
+
+module.exports = {
+    getAll,
+    getOne
+}
