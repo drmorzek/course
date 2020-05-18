c18f8a749cf749b70781400e047d4c8197b1cdc7
drmorzek <drmorzek@mail.ru>
2020-05-18 08:37:14 +0300

    task 2

---
+++ task 2/src/server.js
@@ -0,0 +1,29 @@
+//подключение библиотек
+require("dotenv").config();
+const express  = require("express");
+const bodyparser = require("body-parser");
+
+
+
+//установка константы значения порта
+const port = process.env.PORT || 3000;
+
+//установка константы сервера
+const app = express();
+
+//установки body-parser
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+
+//установки роутов
+app.use("/blogs", blogsRouter);
+
+//подключение к БД и запуск сервера на прослушку порта 
+require("./config/mongo")()
+    .then(()=>{
+        app.listen(port, () => {
+            console.log(`Сервер запустился на порту ${port}...`);
+        });
+    });
